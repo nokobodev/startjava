fbf30bdf8e414a05c851b66e68267da18db50187
Kurmanbek Nurmatov <nokobodev@gmail.com>
2019-11-19 10:38:59 +0300

    changes to Lesson_1 section_6

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,7 +1,6 @@
 public class Variable {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		byte numberOfLocalDrives = 2; 
 		short geforceModel = 920;
 		int cpu	= 5200;

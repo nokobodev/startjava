2b169c16f865c94e44c028cc8601d1a754508719
Kurmanbek Nurmatov <nokobodev@gmail.com>
2019-11-19 09:56:06 +0300

    implement Lesson_1 section_6

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,23 @@
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		byte numberOfLocalDrives = 2; 
+		short geforceModel = 920;
+		int cpu	= 5200;
+		long systemType = 64;
+		float ram = 8.0f;
+		double processorFrequency = 2.2;
+		char windowsVersion = '7';
+		boolean activationStatus = true;
+
+		System.out.println(numberOfLocalDrives);
+		System.out.println(geforceModel);
+		System.out.println(cpu);
+		System.out.println(systemType);
+		System.out.println(ram);
+		System.out.println(processorFrequency);
+		System.out.println(windowsVersion);
+		System.out.println(activationStatus);
+	}
+}

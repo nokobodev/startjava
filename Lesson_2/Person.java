a9ad4aa9573cd6df5006136d0f6d9fdd85f1abd0
Kurmanbek Nurmatov <nokobodev@gmail.com>
2019-11-20 17:33:11 +0300

    implement Lesson_2 section_2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+	
+	String sex = "male";
+	String name = "Alex";
+	double height = 1.73;
+	double weight = 73.5;
+	int age = 41;
+
+	void walk() {
+
+	}
+
+	void sit() {
+
+	}
+
+	void run() {
+
+	}
+
+	void speak() {
+
+	}
+
+	void learnJava() {
+
+	}
+}

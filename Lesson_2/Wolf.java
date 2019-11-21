5e4d61ce2dc7090146d844a7821ae44438378713
Kurmanbek Nurmatov <nokobodev@gmail.com>
2019-11-21 11:37:21 +0300

    modified Wolf and WolfTest

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,30 +1,74 @@
 public class Wolf {
 	
-	String sex;
-	String nickname;
-	double weight;
-	int age;
-	String color;
+	private String sex;
+	private String nickname;
+	private double weight;
+	private int age;
+	private String color;
 
-	void walk() {
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		if (age > 8) {
+			System.out.println("Incorrect age!");
+		} else {
+			this.age = age;
+		}		
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public void walk() {
 		System.out.println("I'm walking!");
 	}
 
-	void sit() {
+	public void sit() {
 		System.out.println("I'm sitting!");
 	}
 
-	void run() {
+	public void run() {
 		System.out.println("I'm running!");
 	}
 
-	String howl() {
+	public String howl() {
 		String someHowl = "Wooo-wooo-wooo!!!";
 		System.out.print("This howl heard from PRINTLN: " + someHowl + " ");
 		return "This howl heard from RETURN: " + someHowl + " :)";
 	}
 
-	void hunt() {
+	public void hunt() {
 		System.out.println("I'm hunting!");
 	}
 }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,17 +2,17 @@ public class WolfTest {
 
 	public static void main(String[] args) {
 		Wolf wolfOne = new Wolf();
-		wolfOne.sex = "male";
-		wolfOne.nickname = "Akela";
-		wolfOne.weight = 40.5;
-		wolfOne.age = 5;
-		wolfOne.color = "grey";
+		wolfOne.setSex("male");
+		wolfOne.setNickname("Akela");
+		wolfOne.setWeight(40.5);
+		wolfOne.setAge(9);
+		wolfOne.setColor("grey");
 
-		System.out.println("Wolf is a " + wolfOne.sex + ".");
-		System.out.println("Wolf's nickname: " + wolfOne.nickname);
-		System.out.println("Wolf's weight: " + wolfOne.weight + " kg.");
-		System.out.println("Wolf's age: " + wolfOne.age);
-		System.out.println("Wolf's color: " + wolfOne.color);
+		System.out.println("Wolf is a " + wolfOne.getSex() + ".");
+		System.out.println("Wolf's nickname: " + wolfOne.getNickname());
+		System.out.println("Wolf's weight: " + wolfOne.getWeight() + " kg.");
+		System.out.println("Wolf's age: " + wolfOne.getAge());
+		System.out.println("Wolf's color: " + wolfOne.getColor());
 		System.out.println();	// Just a separator in console
 
 		wolfOne.walk();
